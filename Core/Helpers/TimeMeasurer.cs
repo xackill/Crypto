785070439ef1c002123eadd7da2a6e2d4d2d689a
xackill <xackill@gmail.com>
2017-03-24 13:00:28 +0500

    Доработки Core
    +Измеритель времени операций

---
+++ Core/Helpers/TimeMeasurer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using Core.Extensions;
+
+namespace Core.Helpers
+{
+    public class TimeMeasurer
+    {
+        private readonly ConcurrentDictionary<string, string> operationsTimes;
+
+        public TimeMeasurer()
+        {
+            operationsTimes = new ConcurrentDictionary<string, string>();
+        }
+
+        public OperationTimeMeasurer StartOperation(string operation)
+            => new OperationTimeMeasurer(operationsTimes, operation);
+
+        public string Results
+            => operationsTimes.Select(g => $"{g.Key} -- {g.Value}").JoinStrings("\n");
+    }
+}
